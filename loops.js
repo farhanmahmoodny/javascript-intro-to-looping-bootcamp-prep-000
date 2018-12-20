1983568f5bf6a59ed3a968f96769825e25a95e41
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-20 21:44:19 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    array.push(`I am ${i} strange loop${i===1 ? '' : 's'}.`)
+  }
+  return array
+}
+
+
+function whileLoop(n){
+  let countdown = 10
+  while (countdown > 0){
+     console.log(--countdown)
+  }
+  return 'done'
+}
+
+
+function doWhileLoop(array){
+  var i = 0;
+  function incrementVariable(){
+    i = i + 1
+  }
+  do{
+    console.log('Do')
+    incrementVariable()
+  }
+  while(i < 5)
+}
